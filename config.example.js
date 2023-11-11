bab97dffe1f0707a7428066954582092126fc79a
The Color Blocks <thecolorblocks@proton.me>
2023-11-11 15:30:36 +0800

    Revert "Added address endpoint"
    This reverts commit ecf27faea15145146f6f310004c06cb4ad3bf619.

--- config.example.js
+++ config.example.js
@@ -1,9 +1,6 @@
 const config = {
   baseurl: "https://api.geniidata.com/api/1/sns",
   apiKey: "api-key",
-  obBaseUrl: 'https://api.ordinalsbot.com',
-  unisatBaseUrl: 'https://open-api.unisat.io',
-  unisatApiKey: ''
 }
 
 export default config
