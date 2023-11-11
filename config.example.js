ecf27faea15145146f6f310004c06cb4ad3bf619
Yibo Yu <yibo@Yibos-MacBook-Pro.local>
2023-11-11 15:28:27 +0800

    Added address endpoint

--- config.example.js
+++ config.example.js
@@ -1,6 +1,9 @@
 const config = {
   baseurl: "https://api.geniidata.com/api/1/sns",
   apiKey: "api-key",
+  obBaseUrl: 'https://api.ordinalsbot.com',
+  unisatBaseUrl: 'https://open-api.unisat.io',
+  unisatApiKey: ''
 }
 
 export default config
